b48e5f97c22f9cc1fcbfd31e30e259243f0b4c19
braian-bernatto <braian5353@gmail.com>
2023-09-03 23:29:35 -0400

    clock icon added for date

--- app/(root)/fixtures/[fixtureId]/components/cell-date.tsx
+++ app/(root)/fixtures/[fixtureId]/components/cell-date.tsx
@@ -1,12 +1,18 @@
 import { format, parseISO } from 'date-fns'
 import { FixtureDetailsColumn } from './columns'
+import { Clock3 } from 'lucide-react'
 
 interface CellDateProps {
   data: FixtureDetailsColumn
 }
 
 const CellDate = ({ data }: CellDateProps) => {
-  return format(parseISO(data.date), 'dd/MM/yyyy | HH:mm') + ' Hs'
+  return (
+    <span className="flex gap-1">
+      {format(parseISO(data.date), 'dd/MM/yyyy | HH:mm')}
+      <Clock3 width={18} className="text-muted-foreground" />
+    </span>
+  )
 }
 
 export default CellDate
